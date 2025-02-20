27bdaf96e1b42d8e864d79f0ff61df90ceec9243
Daniel Hanke <50532296+DanielHanke@users.noreply.github.com>
2025-02-20 18:17:15 -0300

    nuevo ejemplo de obtencion de api

---
+++ Web/Json/dolar.ps1
@@ -0,0 +1,15 @@
+﻿$URIOficial = "https://dolarapi.com/v1/dolares/oficial"
+$URIBlue = "https://dolarapi.com/v1/dolares/blue"
+
+$dolarOficial = Invoke-RestMethod -Method get -Uri $URIOficial
+$dolarBlue = Invoke-RestMethod -Method get -Uri $URIBlue
+ 
+$promedioOficial = ( $dolarOficial.compra + $dolarOficial.venta ) / 2 
+$promedioBlue = ( $dolarBlue.compra + $dolarBlue.venta ) / 2 
+$diferenciaCambiaria = $promedioBlue - $promedioOficial
+$brechaCambiaria = [math]::Round(($diferenciaCambiaria / $promedioOficial) * 100,2)
+
+$line = "`r`nDia " + $dolarOficial.fechaActualizacion + "`r`n Dolar blue (pesos x dolar):    " + $promedioBlue + "`r`n Dolar oficial (pesos x dolar): " +
+        $promedioOficial + "`r`n Brecha cambiaria (%): " + $brechaCambiaria
+
+Write-Output $line 
